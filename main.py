424512b865604b1dbc8a02bc9a44d539f8963c11
wangshub <rocksong.hit@gmail.com>
2020-03-25 10:36:59 +0800

    improvement: test single stock

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-import gym
-import json
-import datetime as dt
-
+import pandas as pd
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
-
 from rlenv.StockTradingEnv0 import StockTradingEnv
 
-import pandas as pd
+stock = 'sz.000725.京东方A.csv'
 
-df = pd.read_csv('./stockdata/train/sz.000725.京东方A.csv')
-# df = pd.read_csv('./data/AAPL.csv')
+df = pd.read_csv(f'./stockdata/train/{stock}')
 df = df.sort_values('date')
 
 # The algorithms require a vectorized environment to run
@@ -20,6 +15,7 @@ env = DummyVecEnv([lambda: StockTradingEnv(df)])
 model = PPO2(MlpPolicy, env, verbose=1)
 model.learn(total_timesteps=20000)
 
+df_test = pd.read_csv(f'./stockdata/test/{stock}')
 obs = env.reset()
 for i in range(2000):
     action, _states = model.predict(obs)
